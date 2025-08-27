b1609821256d7a1a1a41f1544d3a513446841803
coppestj <rovertjc23@gmail.com>
2025-08-26 22:03:43 -0400

    small changes

---
+++ CSE386/exercisebasicgraphics.cpp
@@ -0,0 +1,32 @@
+/****************************************************
+ * 2016-2025 Eric Bachmann and Mike Zmuda
+ * All Rights Reserved.
+ * NOTICE:
+ * Dissemination of this information or reproduction
+ * of this material is prohibited unless prior written
+ * permission is granted.
+ ****************************************************/
+
+#include <ctime>
+#include <vector>
+#include "defs.h"
+#include "utilities.h"
+#include "framebuffer.h"
+#include "colorandmaterials.h"
+#include "rasterization.h"
+#include "io.h"
+
+FrameBuffer frameBuffer(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+void render(GLFWwindow* window) {
+	frameBuffer.clearColorBuffer();
+	drawLine(frameBuffer, 0, 0, 100, 100, red);
+	drawLine(frameBuffer, 100, 100, 200, 100, blue);
+	frameBuffer.showColorBuffer();
+}
+
+int main(int argc, char* argv[]) {
+	frameBuffer.setClearColor(paleGreen);
+	initGraphics(WINDOW_WIDTH, WINDOW_HEIGHT, username.c_str(), render, nullptr, keyboardUtility, nullptr);
+	return 0;
+}
